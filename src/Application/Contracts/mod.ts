00a0d582886d6c7ef21ff9edeb026601ecea7ffc
layton <layton.berth@student.howest.be>
2024-11-29 23:17:03 +0100

    Add RoomQueryAllDto and RoomQueryRepository interfaces to Contracts module

--- src/Application/Contracts/mod.ts
+++ src/Application/Contracts/mod.ts
@@ -3,3 +3,8 @@ export type { OutputPort } from "./Ports/OutputPort.ts";
 
 export type { ShowRoomsInput } from "./ShowRoomsInput.ts";
 export type { ShowRoomsOutput } from "./ShowRoomsOutput.ts";
+
+export type {
+    RoomQueryAllDto,
+    RoomQueryRepository,
+} from "./Ports/Repositories/RoomQueryRepository.ts";
