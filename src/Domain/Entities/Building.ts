0e5b2cf82838f63a13f778302e20d49892d824f1
layton <layton.berth@student.howest.be>
2024-11-29 21:56:23 +0100

    Implement Building class with validation and GUID generation; refactor BuildingState interface

--- src/Domain/Entities/Building.ts
+++ src/Domain/Entities/Building.ts
@@ -1,9 +1,80 @@
-import { Room } from 'EnviroSense/Domain/mod.ts';
+import { Room, Guid } from "EnviroSense/Domain/mod.ts";
+import { DomainException } from "EnviroSense/Domain/Shared/Exceptions/DomainException.ts";
 
-export interface Building {
-    id: string;
+export interface BuildingState {
+    id: Guid;
     name: string;
     address: string;
     rooms?: Room[];
 }
 
+export class Building {
+    private readonly _id: Guid;
+    private readonly _name: string;
+    private readonly _address: string;
+    private readonly _rooms: Room[];
+
+    private constructor(
+        id: Guid,
+        name: string,
+        address: string,
+        rooms: Room[]
+    ) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+        this._rooms = rooms;
+    }
+
+    static create(name: string, address: string, rooms: Room[]): Building {
+        const building = new Building(Guid.create(), name, address, rooms);
+        building.validateState();
+
+        return building;
+    }
+
+    static load(state: BuildingState): Building {
+        const tournament = new Building(
+            state.id,
+            state.name,
+            state.address,
+            state.rooms || []
+        );
+        tournament.validateState();
+
+        return tournament;
+    }
+
+    public validateState() {
+        this.ensureNameIsNotEmpty();
+        this.ensureAddressIsNotEmpty();
+    }
+
+    private ensureNameIsNotEmpty() {
+        if (!this._name) {
+            throw new DomainException("Name is required");
+        }
+    }
+
+    private ensureAddressIsNotEmpty() {
+        if (!this._address) {
+            throw new DomainException("Address is required");
+        }
+    }
+
+    get id(): Guid {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get address(): string {
+        return this._address;
+    }
+
+    get rooms(): Room[] {
+        return this._rooms;
+    }
+}
--- src/Domain/mod.ts
+++ src/Domain/mod.ts
@@ -1,11 +1,14 @@
-export { Optional } from 'EnviroSense/Domain/Shared/Optional.ts';
-export { Exception } from 'EnviroSense/Domain/Shared/Exceptions/Exception.ts';
-export { DomainException } from 'EnviroSense/Domain/Shared/Exceptions/DomainException.ts';
-export { NotFoundException } from 'EnviroSense/Domain/Shared/Exceptions/NotFoundException.ts';
-export { IllegalStateException } from 'EnviroSense/Domain/Shared/Exceptions/IllegalStateException.ts';
+export { Guid } from "EnviroSense/Domain/Shared/Guid.ts";
+export { Optional } from "EnviroSense/Domain/Shared/Optional.ts";
+export { Exception } from "EnviroSense/Domain/Shared/Exceptions/Exception.ts";
+export { DomainException } from "EnviroSense/Domain/Shared/Exceptions/DomainException.ts";
+export { NotFoundException } from "EnviroSense/Domain/Shared/Exceptions/NotFoundException.ts";
+export { IllegalStateException } from "EnviroSense/Domain/Shared/Exceptions/IllegalStateException.ts";
 
-export type { Building } from 'EnviroSense/Domain/Entities/Building.ts'
-export type { Device } from 'EnviroSense/Domain/Entities/Device.ts'
-export type { DeviceData } from 'EnviroSense/Domain/Entities/DeviceData.ts'
-export type { Room } from 'EnviroSense/Domain/Entities/Room.ts'
-export type { RoomType } from 'EnviroSense/Domain/Entities/RoomType.ts'
+export { Building } from "EnviroSense/Domain/Entities/Building.ts";
+export type { BuildingState } from "EnviroSense/Domain/Entities/Building.ts";
+
+export type { Device } from "EnviroSense/Domain/Entities/Device.ts";
+export type { DeviceData } from "EnviroSense/Domain/Entities/DeviceData.ts";
+export type { Room } from "EnviroSense/Domain/Entities/Room.ts";
+export type { RoomType } from "EnviroSense/Domain/Entities/RoomType.ts";
