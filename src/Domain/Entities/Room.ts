9602c06e3de7e586d9316110684e6b21b57e938f
layton <layton.berth@student.howest.be>
2024-11-29 22:22:56 +0100

    Implement Room class with validation and device management; rename Room interface to RoomState and add methods for device handling

--- src/Domain/Entities/Room.ts
+++ src/Domain/Entities/Room.ts
@@ -1,9 +1,132 @@
-import { Building, Device, Guid, RoomType } from 'EnviroSense/Domain/mod.ts';
+import {
+    Building,
+    Device,
+    DomainException,
+    Guid,
+    RoomType,
+} from "EnviroSense/Domain/mod.ts";
 
-export interface Room {
+export interface RoomState {
     id: Guid;
     name: string;
     building: Building;
     roomType: RoomType;
     devices?: Device[];
+}
+
+export class Room {
+    private readonly _id: Guid;
+    private readonly _name: string;
+    private readonly _building: Building;
+    private readonly _roomType: RoomType;
+    private _devices: Device[];
+
+    private constructor(
+        id: Guid,
+        name: string,
+        building: Building,
+        roomType: RoomType,
+        devices: Device[]
+    ) {
+        this._id = id;
+        this._name = name;
+        this._building = building;
+        this._roomType = roomType;
+        this._devices = devices;
+    }
+
+    static create(
+        name: string,
+        building: Building,
+        roomType: RoomType,
+        devices: Device[]
+    ): Room {
+        const room = new Room(Guid.create(), name, building, roomType, devices);
+        room.validateState();
+
+        return room;
+    }
+
+    static load(state: RoomState): Room {
+        const room = new Room(
+            state.id,
+            state.name,
+            state.building,
+            state.roomType,
+            state.devices || []
+        );
+        room.validateState();
+
+        return room;
+    }
+
+    public validateState() {
+        this.ensureNameIsNotEmpty();
+        this.ensureBuildingIsNotEmpty();
+        this.ensureRoomTypeIsNotEmpty();
+    }
+
+    public addDevice(device: Device) {
+        this.ensureDeviceDoesNotExist(device);
+
+        this._devices.push(device);
+    }
+
+    public removeDevice(deviceId: Guid) {
+        this.ensureDeviceExists(deviceId);
+
+        this._devices = this._devices.filter(
+            (device) => device.id !== deviceId
+        );
+    }
+
+    private ensureNameIsNotEmpty() {
+        if (!this._name) {
+            throw new DomainException("Room name cannot be empty.");
+        }
+    }
+
+    private ensureRoomTypeIsNotEmpty() {
+        if (!this._roomType) {
+            throw new DomainException("Room type cannot be empty.");
+        }
+    }
+
+    private ensureBuildingIsNotEmpty() {
+        if (!this._building) {
+            throw new DomainException("Building cannot be empty.");
+        }
+    }
+
+    private ensureDeviceDoesNotExist(device: Device) {
+        if (this._devices.some((d) => d.id === device.id)) {
+            throw new DomainException("Device already exists in this room.");
+        }
+    }
+
+    private ensureDeviceExists(deviceId: Guid) {
+        if (!this._devices.some((d) => d.id === deviceId)) {
+            throw new DomainException("Device does not exist in this room.");
+        }
+    }
+
+    get id(): Guid {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get building(): Building {
+        return this._building;
+    }
+
+    get roomType(): RoomType {
+        return this._roomType;
+    }
+
+    get devices(): Device[] {
+        return this._devices;
+    }
 }
