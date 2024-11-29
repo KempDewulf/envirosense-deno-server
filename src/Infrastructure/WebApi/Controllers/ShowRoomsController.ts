6aea4ec5b286e294ac31a77d06d614a59f0d55b9
layton <layton.berth@student.howest.be>
2024-11-29 22:48:03 +0100

    Add ShowRoomsController to handle room display requests

---
+++ src/Infrastructure/WebApi/Controllers/ShowRoomsController.ts
@@ -0,0 +1,26 @@
+import { Controller } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import {
+    ShowRoomsInput,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export interface ShowRoomsRequest {
+    type: string;
+}
+
+export class ShowRoomsController implements Controller<ShowRoomsRequest> {
+    private readonly _useCase: UseCase<ShowRoomsInput>;
+
+    constructor(useCase: UseCase<ShowRoomsInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(request: ShowRoomsRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: ShowRoomsRequest): ShowRoomsInput {
+        return { type: request.type };
+    }
+}
