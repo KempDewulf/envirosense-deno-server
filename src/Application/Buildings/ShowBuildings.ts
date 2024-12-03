9c27ca5d1912587116d37628beafecec82e81d2c
Layton Berth <laytonbrth@gmail.com>
2024-12-03 21:21:07 +0100

    Implement ShowBuildings use case with output mapping and building retrieval

---
+++ src/Application/Buildings/ShowBuildings.ts
@@ -0,0 +1,35 @@
+import {
+    OutputPort,
+    ShowBuildingsInput,
+    ShowBuildingsOutput,
+    BuildingQueryAllDto,
+    BuildingQueryRepository,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export class ShowBuildings implements UseCase<ShowBuildingsInput> {
+    private readonly _outputPort: OutputPort<ShowBuildingsOutput[]>;
+    private readonly _buildingQueryRepository: BuildingQueryRepository;
+
+    constructor(
+        outputPort: OutputPort<ShowBuildingsOutput[]>,
+        buildingQueryRepository: BuildingQueryRepository
+    ) {
+        this._outputPort = outputPort;
+        this._buildingQueryRepository = buildingQueryRepository;
+    }
+
+    public async execute(input: ShowBuildingsInput): Promise<void> {
+        const buildingsDto = await this._buildingQueryRepository.all(input.name);
+        const buildings = this.mapDtoToOutput(buildingsDto);
+        this._outputPort.present(buildings);
+    }
+
+    private mapDtoToOutput(dto: BuildingQueryAllDto[]): ShowBuildingsOutput[] {
+        return dto.map((item) => ({
+            id: item.id,
+            name: item.name,
+            address: item.address
+        }));
+    }
+}
