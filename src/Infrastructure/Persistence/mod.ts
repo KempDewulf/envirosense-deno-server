af7b59db93e4337d23a59543fe1e1f4e9330e272
Layton Berth <laytonbrth@gmail.com>
2024-12-03 21:27:42 +0100

    Add ShowBuildingsEndpoint and update persistence exports for building data retrieval

--- src/Infrastructure/Persistence/mod.ts
+++ src/Infrastructure/Persistence/mod.ts
@@ -1,3 +1,7 @@
+export { StrapiQueryRepository } from "EnviroSense/Infrastructure/Persistence/Shared/StrapiQueryRepository.ts";
+
 export { RoomStrapiRepository } from "./Repositories/Strapi/Room/RoomStrapiRepository.ts";
 export { RoomStrapiQueryRepository } from "./Repositories/Strapi/Room/RoomStrapiQueryRepository.ts";
-export { StrapiQueryRepository } from "EnviroSense/Infrastructure/Persistence/Shared/StrapiQueryRepository.ts";
+
+export { BuildingStrapiRepository } from "./Repositories/Strapi/Building/BuildingStrapiRepository.ts";
+export { BuildingStrapiQueryRepository } from "./Repositories/Strapi/Building/BuildingStrapiQueryRepository.ts";
