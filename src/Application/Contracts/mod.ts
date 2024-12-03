943e5ca981e0280649fa5e30e6c26ea6c45647a6
Layton Berth <laytonbrth@gmail.com>
2024-12-03 21:19:02 +0100

    Add building-related types and exports to Contracts module; include ShowBuildings input/output and BuildingQueryRepository.

--- src/Application/Contracts/mod.ts
+++ src/Application/Contracts/mod.ts
@@ -1,6 +1,7 @@
 export type { UseCase } from "./Ports/UseCase.ts";
 export type { OutputPort } from "./Ports/OutputPort.ts";
 
+//Rooms
 export type { ShowRoomsInput } from "./ShowRoomsInput.ts";
 export type { ShowRoomsOutput } from "./ShowRoomsOutput.ts";
 
@@ -10,3 +11,14 @@ export type {
 } from "./Ports/Repositories/Room/RoomQueryRepository.ts";
 
 export type { RoomRepository } from "./Ports/Repositories/Room/RoomRepository.ts";
+
+//Buildings
+export type { ShowBuildingsInput } from "./ShowBuildingsInput.ts";
+export type { ShowBuildingsOutput } from "./ShowBuildingsOutput.ts";
+
+export type {
+    BuildingQueryAllDto,
+    BuildingQueryRepository,
+} from "./Ports/Repositories/Building/BuildingQueryRepository.ts";
+
+export type { BuildingRepository } from "./Ports/Repositories/Building/BuildingRepository.ts";
--- src/Application/mod.ts
+++ src/Application/mod.ts
@@ -1,1 +1,2 @@
 export { ShowRooms } from "EnviroSense/Application/Rooms/ShowRooms.ts";
+export { ShowBuildings } from "EnviroSense/Application/Buildings/ShowBuildings.ts";
