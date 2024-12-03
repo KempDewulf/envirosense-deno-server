8a37445bfc237bd60592ba67fc425d7d59d1d7a8
Layton Berth <laytonbrth@gmail.com>
2024-12-03 21:25:58 +0100

    Implement ShowBuildingsController to handle building requests and map inputs for use case execution

---
+++ src/Infrastructure/WebApi/Controllers/ShowBuildingsController.ts
@@ -0,0 +1,26 @@
+import { Controller } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import {
+    ShowBuildingsInput,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export interface ShowBuildingsRequest {
+    name: string;
+}
+
+export class ShowBuildingsController implements Controller<ShowBuildingsRequest> {
+    private readonly _useCase: UseCase<ShowBuildingsInput>;
+
+    constructor(useCase: UseCase<ShowBuildingsInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(request: ShowBuildingsRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: ShowBuildingsRequest): ShowBuildingsInput {
+        return { name: request.name };
+    }
+}
