164fb7be359ffb810bcc44cff54bd8dd336bfa04
Layton Berth <laytonbrth@gmail.com>
2024-12-03 19:52:05 +0100

    Remove createRoom function and related room creation logic from start.ts

--- src/Main/start.ts
+++ src/Main/start.ts
@@ -1,21 +1,3 @@
 import { WebApiModule } from 'EnviroSense/Infrastructure/WebApi/mod.ts';
-import { RoomStrapiRepository } from 'EnviroSense/Infrastructure/Persistence/mod.ts';
-import { Building, Room, RoomType } from 'EnviroSense/Domain/mod.ts';
 
 (new WebApiModule(8101)).run();
-
-async function createRoom(): Promise<any> {
-    const repo = new RoomStrapiRepository();
-
-    let building = await fetch('http://94.130.75.173:1331/api/buildings/gox5y6bsrg640qb11ak44dh0?populate=*').then(res => res.json());
-    building = Building.create(building.data.name, building.data.address);
-
-    let roomType = await fetch('http://94.130.75.173:1331/api/room-types/hd3f8ql67kwhzmbqllumnbdi?populate=*').then(res => res.json());
-    roomType = RoomType.create(roomType.data.name, roomType.data.icon.name);
-
-    const roomModel = Room.create('Test Classroom', building, roomType);
-    return await repo.save(roomModel);
-}
-
-const room = await createRoom();
-console.log(room);
