fedffe90e0502cd579e7e78eb42b0e739d4c4f7b
Layton Berth <laytonbrth@gmail.com>
2024-12-03 21:24:26 +0100

    Refactor ShowBuildings and ShowRooms use cases: move to dedicated folders and update imports; implement new BuildingStrapiQueryRepository for building data retrieval.

--- src/Application/mod.ts
+++ src/Application/mod.ts
@@ -1,2 +1,2 @@
-export { ShowRooms } from "EnviroSense/Application/Rooms/ShowRooms.ts";
-export { ShowBuildings } from "EnviroSense/Application/Buildings/ShowBuildings.ts";
+export { ShowRooms } from "./Room/ShowRooms.ts";
+export { ShowBuildings } from "./Building/ShowBuildings.ts";
---
+++ src/Infrastructure/Persistence/Repositories/Strapi/Building/BuildingStrapiQueryRepository.ts
@@ -0,0 +1,27 @@
+import {
+    BuildingQueryRepository,
+    BuildingQueryAllDto,
+} from "EnviroSense/Application/Contracts/mod.ts";
+import { StrapiQueryRepository } from "../../../Shared/StrapiQueryRepository.ts";
+
+export class BuildingStrapiQueryRepository
+    extends StrapiQueryRepository
+    implements BuildingQueryRepository
+{
+    async all(name: string): Promise<BuildingQueryAllDto[]> {
+        const endpoint = 'buildings';
+        const params = name ? { 'filters[name][$contains]': name, 'populate': '*' } : undefined;
+        const response = await this.get<any>(endpoint, params);
+
+        const buildings = response.data.map((item: any) => this.mapToDto(item));
+        return buildings;
+    }
+
+    private mapToDto(item: any): BuildingQueryAllDto {
+        return {
+            id: item.id,
+            name: item.name,
+            address: item.address
+        };
+    }
+}
