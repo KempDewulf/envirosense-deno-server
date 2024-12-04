1bb92e376779d6a9c9d9f6c2840d8a952930c060
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-04 10:51:06 +0100

    Implement MQTT messaging functionality with connect, subscribe, and publish methods

--- src/Infrastructure/Messaging/Messaging.ts
+++ src/Infrastructure/Messaging/Messaging.ts
@@ -1,10 +1,44 @@
-import { Module } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import { Client } from "https://deno.land/x/mqtt@0.1.2/deno/mod.ts";
+import { DeviceData } from "EnviroSense/Domain/Entities/DeviceData.ts";
+//import { DeviceDataStrapiRepository } from "EnviroSense/Infrastructure/Persistence/Repositories/Strapi/DeviceData/DeviceDataStrapiRepository.ts";
 
-export class Messaging implements Module {
-    run(): Promise<void> {
-        console.log('Messaging running');
-        return new Promise(() => {
-            // Add your logic here
+
+export class Messaging {
+    private client: Client;
+    //private repository: DeviceDataStrapiRepository;
+
+    constructor() {
+        this.client = new Client({url: 'mqtt://94.130.75.173:1883', username: "server", password: "PdtwMva9833Z"});
+    }
+
+    public async connect(): Promise<void> {
+        await this.client.connect();
+        //this.repository = new DeviceDataStrapiRepository();
+    }
+
+    public async subscribe(topic: string): Promise<void> {
+        await this.client.subscribe(topic);
+        this.client.on('message', (topic, payload) => {
+            const msg = new TextDecoder().decode(payload);
+            console.log(`Received message: ${msg} from topic: ${topic}`);
+
+            const data = JSON.parse(msg);
+            const deviceData = DeviceData.create(
+                data.id,
+                data.deviceId,
+                new Date(),
+                data.temperature,
+                data.humidity,
+                data.gasPpm
+            );
+
+            //this.repository.save(deviceData).then();
+
         });
     }
+
+    public async publish(topic: string, message: string): Promise<void> {
+        await this.client.publish(topic, message);
+    }
 }
+
--- src/Main/start.ts
+++ src/Main/start.ts
@@ -1,12 +1,21 @@
 import { WebApiModule } from 'EnviroSense/Infrastructure/WebApi/mod.ts';
 import { BuildingStrapiQueryRepository, BuildingStrapiRepository, RoomStrapiQueryRepository } from 'EnviroSense/Infrastructure/Persistence/mod.ts';
 import { Building } from 'EnviroSense/Domain/mod.ts';
+import { Messaging } from 'EnviroSense/Infrastructure/Messaging/mod.ts';
 
 (new WebApiModule(8101)).run();
 
 const repo = new BuildingStrapiRepository();
 const repoQuery = new BuildingStrapiQueryRepository();
 const repoQueryRooms = new RoomStrapiQueryRepository();
+const mqttClient = new Messaging();
+
+async function logMessages() {
+    await mqttClient.connect();
+    await mqttClient.subscribe('test/test1');
+}
+
+//logMessages();
 
 async function createBuilding(): Promise<any> {
     const buildingModel = Building.create('', 'Building Test', 'Building test description');
