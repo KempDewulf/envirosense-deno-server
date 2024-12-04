c2d6ce794d59c49c970645de5d7996ddf42947a6
layton <layton.berth@student.howest.be>
2024-12-04 19:34:30 +0100

    finish controller

--- src/Infrastructure/WebApi/Controllers/ShowRoomTypeByDocumentIdController.ts
+++ src/Infrastructure/WebApi/Controllers/ShowRoomTypeByDocumentIdController.ts
@@ -1,1 +1,32 @@
-//TODO: implement controller
+import { Controller } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import {
+    ShowRoomTypeByDocumentIdInput,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export interface ShowRoomTypeByDocumentIdRequest {
+    name: string;
+}
+
+export class ShowRoomTypeByDocumentIdController
+    implements Controller<ShowRoomTypeByDocumentIdRequest>
+{
+    private readonly _useCase: UseCase<ShowRoomTypeByDocumentIdInput>;
+
+    constructor(useCase: UseCase<ShowRoomTypeByDocumentIdInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(
+        request: ShowRoomTypeByDocumentIdRequest
+    ): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(
+        request: ShowRoomTypeByDocumentIdRequest
+    ): ShowRoomTypeByDocumentIdInput {
+        return { name: request.name };
+    }
+}
