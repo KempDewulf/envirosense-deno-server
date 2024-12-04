e0729b85aa0b0e0f725589148d5e7ee5782147ff
layton <layton.berth@student.howest.be>
2024-12-04 10:21:35 +0100

    Add ShowDevicesPresenter for formatting device output data

---
+++ src/Infrastructure/WebApi/Presenters/ShowDevicesPresenter.ts
@@ -0,0 +1,39 @@
+import { RequestResponseDevice } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import {
+    OutputPort,
+    ShowDevicesOutput,
+} from "EnviroSense/Application/Contracts/mod.ts";
+import { DeviceData, Room } from 'EnviroSense/Domain/mod.ts';
+
+export type ShowDevicesPresentedData = {
+    id: string;
+    documentId: string;
+    identifier: string;
+    room: Room;
+    device_data: DeviceData[];
+};
+
+export class ShowDevicesPresenter implements OutputPort<ShowDevicesOutput[]> {
+    private readonly _device: RequestResponseDevice<ShowDevicesPresentedData[]>;
+
+    constructor(device: RequestResponseDevice<ShowDevicesPresentedData[]>) {
+        this._device = device;
+    }
+
+    present(data: ShowDevicesOutput[]): void {
+        const presentedData = this.mapToPresentedData(data);
+        this._device.update(presentedData);
+    }
+
+    protected mapToPresentedData(
+        data: ShowDevicesOutput[]
+    ): ShowDevicesPresentedData[] {
+        return data.map((device: ShowDevicesOutput) => ({
+            id: device.id,
+            documentId: device.documentId,
+            identifier: device.identifier,
+            room: device.room,
+            device_data: device.device_data
+        }));
+    }
+}
