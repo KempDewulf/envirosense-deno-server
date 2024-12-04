c2aae7b9d5ca6e9fb5dfbb07f6dea50c2ed16f12
layton <layton.berth@student.howest.be>
2024-12-04 19:26:20 +0100

    structure + add showroomtypebydocumentId

--- src/Application/mod.ts
+++ src/Application/mod.ts
@@ -1,5 +1,15 @@
+// Room
 export { ShowRooms } from "./Room/ShowRooms.ts";
+
+// Building
 export { ShowBuildings } from "./Building/ShowBuildings.ts";
+
+// Device
 export { ShowDevices } from "./Device/ShowDevice.ts";
+
+// Room Type
 export { ShowRoomTypes } from "./RoomType/ShowRoomType.ts";
+export { ShowRoomTypeByDocumentId } from "./Room/ShowRoomTypeByDocumentId.ts";
+
+// Device Data
 export { ShowDeviceData } from "./DeviceData/ShowDeviceData.ts";
