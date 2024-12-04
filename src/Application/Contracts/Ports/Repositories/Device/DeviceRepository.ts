ddc3046048ee384ae177f947e4c7abd7bad2b5df
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-04 13:26:05 +0100

    Add findByIdentifier method to DeviceRepository for fetching devices by identifier

--- src/Application/Contracts/Ports/Repositories/Device/DeviceRepository.ts
+++ src/Application/Contracts/Ports/Repositories/Device/DeviceRepository.ts
@@ -2,5 +2,6 @@ import { Optional, Device } from "EnviroSense/Domain/mod.ts";
 
 export interface BuildingRepository {
     find(deviceId: string): Promise<Optional<Device>>;
+    findByIdentifier(identifier: string): Promise<Optional<Device>>;
     save(device: Device): Promise<void>;
 }
