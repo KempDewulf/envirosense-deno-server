1898f05c8ae334a45875aa80e1b0e40537950de2
layton <layton.berth@student.howest.be>
2024-12-05 14:31:18 +0100

    remove useless import

--- src/Application/RoomType/ShowRoomTypeByDocumentId.ts
+++ src/Application/RoomType/ShowRoomTypeByDocumentId.ts
@@ -4,7 +4,6 @@ import {
     ShowRoomTypeByDocumentIdInput,
     ShowRoomTypeByDocumentIdOutput,
     RoomTypeQueryByDocumentIdDto,
-    RoomTypeQueryRepository,
     RoomTypeRepository,
 } from "EnviroSense/Application/Contracts/mod.ts";
 
