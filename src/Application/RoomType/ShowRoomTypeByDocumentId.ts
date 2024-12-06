b341b36ba8eca7d06d4617ce02a526a7e685fc95
layton <layton.berth@student.howest.be>
2024-12-06 09:27:11 +0100

    remove commented-out code and TODO notes in ShowRoomTypeByDocumentId

--- src/Application/RoomType/ShowRoomTypeByDocumentId.ts
+++ src/Application/RoomType/ShowRoomTypeByDocumentId.ts
@@ -25,14 +25,6 @@ export class ShowRoomTypeByDocumentId
         const roomTypeDto = await this._roomTypeRepository.find(
             input.roomTypeDocumentId
         );
-
-        //TODO: ask dimitri about this since there is no example online and i can't figure it out myself or ai
-        /*
-        Argument of type 'Optional<RoomType>' is not assignable to parameter of type 'RoomTypeQueryByDocumentIdDto'.
-  Type 'Optional<RoomType>' is missing the following properties from type 'RoomTypeQueryByDocumentIdDto': id, documentId, name, icon, rooms
-        */
-        // I know why it doesn't work but i don't know how I can fix it since we can reuse the find from other repo
-        //because it has Optional<RoomType> as return type for other features and now we need to only send the DTO?
         const roomType = this.mapDtoToOutput(roomTypeDto);
         this._outputPort.present(roomType);
     }
