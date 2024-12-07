069049af105f2c43f19a481980dc646d729ddfab
Layton Berth <laytonbrth@gmail.com>
2024-12-07 02:35:11 +0100

    feat: add CreateRoomTypePresenter for handling room type output presentation

---
+++ src/Infrastructure/WebApi/Presenters/CreateRoomTypePresenter.ts
@@ -0,0 +1,25 @@
+import { RequestResponseDevice } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import { CreateRoomTypeOutput, OutputPort } from 'EnviroSense/Application/Contracts/mod.ts';
+
+export interface CreateRoomTypePresentedData {
+    url: string;
+}
+
+export class CreateRoomTypePresenter implements OutputPort<CreateRoomTypeOutput> {
+    private readonly _device: RequestResponseDevice<CreateRoomTypePresentedData>;
+
+    constructor(device: RequestResponseDevice<CreateRoomTypePresentedData>) {
+        this._device = device;
+    }
+
+    present(data: CreateRoomTypeOutput): void {
+        const presentedData = this.mapToPresentedData(data);
+        this._device.update(presentedData);
+    }
+
+    protected mapToPresentedData(data: CreateRoomTypeOutput): CreateRoomTypePresentedData {
+        return {
+            url: `/room-types/${data.id}`,
+        };
+    }
+}
