fb4cbdc66c242c660033072a59dda9f1cad210f9
Layton Berth <laytonbrth@gmail.com>
2024-12-07 02:17:19 +0100

    refactor: standardize import quotes and reorder endpoint declarations for consistency

--- src/Infrastructure/WebApi/Endpoints.ts
+++ src/Infrastructure/WebApi/Endpoints.ts
@@ -1,13 +1,13 @@
-import { Router, RouterContext } from "@oak/oak";
+import { Router, RouterContext } from '@oak/oak';
 import {
     Endpoint,
-    TestEndpoint,
-    ShowRoomsEndpoint,
-    ShowDeviceDataEndpoint,
     ShowBuildingsEndpoint,
-    ShowRoomTypesEndpoint,
+    ShowDeviceDataEndpoint,
+    ShowRoomsEndpoint,
     ShowRoomTypeByDocumentIdEndpoint,
-} from "EnviroSense/Infrastructure/WebApi/mod.ts";
+    ShowRoomTypesEndpoint,
+    TestEndpoint,
+} from 'EnviroSense/Infrastructure/WebApi/mod.ts';
 
 function use(endpoint: Endpoint) {
     return (context: RouterContext<string>) => endpoint.handle(context);
@@ -16,14 +16,11 @@ function use(endpoint: Endpoint) {
 export function endpoints(): Router {
     const router = new Router();
 
-    router.get("/", use(new TestEndpoint())); //maybe show docs of openApi.yml?
-    router.get("/rooms", use(new ShowRoomsEndpoint()));
-    router.get("/buildings", use(new ShowBuildingsEndpoint()));
-    router.get("/room-types", use(new ShowRoomTypesEndpoint()));
-    router.get(
-        "/room-types/:roomTypeDocumentId",
-        use(new ShowRoomTypeByDocumentIdEndpoint())
-    );
-    router.get("/device-data", use(new ShowDeviceDataEndpoint()));
+    router.get('/', use(new TestEndpoint())); //maybe show docs of openApi.yml?
+    router.get('/rooms', use(new ShowRoomsEndpoint()));
+    router.get('/buildings', use(new ShowBuildingsEndpoint()));
+    router.get('/room-types', use(new ShowRoomTypesEndpoint()));
+    router.get('/room-types/:roomTypeDocumentId',use(new ShowRoomTypeByDocumentIdEndpoint()));
+    router.get('/device-data', use(new ShowDeviceDataEndpoint()));
     return router;
 }
