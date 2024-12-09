95af25588d9331e2af2ca3f738c86dda1d6247bb
Layton Berth <laytonbrth@gmail.com>
2024-12-09 14:58:28 +0100

    fix: reorder room types endpoints for improved clarity and consistency

--- src/Infrastructure/WebApi/Endpoints.ts
+++ src/Infrastructure/WebApi/Endpoints.ts
@@ -20,11 +20,14 @@ export function endpoints(): Router {
 
     router.get('/', use(new TestEndpoint())); //maybe show docs of openApi.yml?
     router.get('/rooms', use(new ShowRoomsEndpoint()));
+
     router.get('/buildings', use(new ShowBuildingsEndpoint()));
+
     router.get('/room-types', use(new ShowRoomTypesEndpoint()));
-    router.post('/room-types', use(new CreateRoomTypeEndpoint()));
     router.get('/room-types/:roomTypeDocumentId', use(new ShowRoomTypeByDocumentIdEndpoint()));
     router.delete('/room-types/:roomTypeDocumentId', use(new DeleteRoomTypeEndpoint()));
+    router.post('/room-types', use(new CreateRoomTypeEndpoint()));
+
     router.get('/device-data', use(new ShowDeviceDataEndpoint()));
     return router;
 }
