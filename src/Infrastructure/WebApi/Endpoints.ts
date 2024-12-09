872ed2b93c0e2c891ac4aa61df62e04f18cb4299
Layton Berth <laytonbrth@gmail.com>
2024-12-09 14:36:04 +0100

    feat: add DeleteRoomType endpoint to WebApi router

--- src/Infrastructure/WebApi/Endpoints.ts
+++ src/Infrastructure/WebApi/Endpoints.ts
@@ -1,6 +1,7 @@
 import { Router, RouterContext } from '@oak/oak';
 import {
     CreateRoomTypeEndpoint,
+    DeleteRoomTypeEndpoint,
     Endpoint,
     ShowBuildingsEndpoint,
     ShowDeviceDataEndpoint,
@@ -23,6 +24,7 @@ export function endpoints(): Router {
     router.get('/room-types', use(new ShowRoomTypesEndpoint()));
     router.post('/room-types', use(new CreateRoomTypeEndpoint()));
     router.get('/room-types/:roomTypeDocumentId', use(new ShowRoomTypeByDocumentIdEndpoint()));
+    router.delete('/room-types/:roomTypeDocumentId', use(new DeleteRoomTypeEndpoint()));
     router.get('/device-data', use(new ShowDeviceDataEndpoint()));
     return router;
 }
