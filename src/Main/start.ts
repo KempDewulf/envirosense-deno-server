0b493817ef86fff31b0258ec726247577ea91933
Layton Berth <laytonbrth@gmail.com>
2024-12-09 14:33:01 +0100

    fix: comment out logMessages function and update TODO for invalid relations error with icon

--- src/Main/start.ts
+++ src/Main/start.ts
@@ -2,19 +2,9 @@ import { WebApiModule } from "EnviroSense/Infrastructure/WebApi/mod.ts";
 import {
     RoomTypeStrapiRepository,
 } from "EnviroSense/Infrastructure/Persistence/mod.ts";
-import { Messaging } from "EnviroSense/Infrastructure/Messaging/mod.ts";
-import { RoomType } from 'EnviroSense/Domain/mod.ts';
 
 new WebApiModule(8101).run();
 
 const repoQuery = new RoomTypeStrapiRepository();
-const mqttClient = new Messaging();
 
-async function logMessages() {
-    await mqttClient.connect();
-    await mqttClient.subscribe("test/test1");
-}
-
-//logMessages();
-
-//TODO: invalid relations error due the icon not passing correctly, since icon is seen as relation...
+//TODO: findById: invalid relations error due the icon not passing correctly, since icon is seen as relation...
