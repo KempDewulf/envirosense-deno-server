e10344e6e9f6cda7ab12a80d4c7b6d547c669960
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-09 17:52:41 +0100

    Refactor Messaging class constructor to accept ProcessAirDataUseCase as a parameter

--- src/Infrastructure/Messaging/Messaging.ts
+++ src/Infrastructure/Messaging/Messaging.ts
@@ -1,37 +1,33 @@
 import { Client } from "https://deno.land/x/mqtt@0.1.2/deno/mod.ts";
 import { AirData } from 'EnviroSense/Domain/mod.ts';
-import { ProcessAirData } from "EnviroSense/Application/mod.ts";
-import { DeviceDataRepository, DeviceRepository, ProcessAirDataInput, UseCase } from "EnviroSense/Application/Contracts/mod.ts";
-import { DeviceStrapiRepository, DeviceDataStrapiRepository} from "EnviroSense/Infrastructure/Persistence/mod.ts";
+import {
+  ProcessAirDataInput,
+  UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
 import "jsr:@std/dotenv/load";
 
-
 export class Messaging {
-    private client: Client;
-    private processAirDataUseCase: UseCase<ProcessAirDataInput>;
+  private client: Client;
+  private processAirDataUseCase: UseCase<ProcessAirDataInput>;
 
-    constructor() {        
-        this.client = new Client({
-            url: Deno.env.get("MQTT_BROKER"), 
-            username: Deno.env.get("MQTT_USERNAME"), 
-            password: Deno.env.get("MQTT_PASSWORD")
-        });
-        const deviceRepository: DeviceRepository = new DeviceStrapiRepository();
-        const deviceDataRepository: DeviceDataRepository = new DeviceDataStrapiRepository();
+  constructor(processAirDataUseCase: UseCase<ProcessAirDataInput>) {
+    this.client = new Client({
+      url: Deno.env.get("MQTT_BROKER"),
+      username: Deno.env.get("MQTT_USERNAME"),
+      password: Deno.env.get("MQTT_PASSWORD"),
+    });
+    this.processAirDataUseCase = processAirDataUseCase;
+  }
 
-        this.processAirDataUseCase = new ProcessAirData(
-            deviceRepository,
-            deviceDataRepository
-        );
-    }
+  public async connect(): Promise<void> {
+    await this.client.connect();
+  }
 
-    public async connect(): Promise<void> {
-        await this.client.connect();
-    }
-
-    public async subscribe(topic: string): Promise<void> {
-        await this.client.subscribe(topic);
-        this.client.on('message', async (topic: string, payload: Uint8Array) => {
+  public async subscribe(topic: string): Promise<void> {
+    await this.client.subscribe(topic);
+    this.client.on(
+        'message', 
+        async (topic: string, payload: Uint8Array) => {
             const msg: string = new TextDecoder().decode(payload);
             const deviceIdentifier: string = this.getDeviceId(topic);
             const airData: AirData = JSON.parse(msg);
@@ -42,15 +38,15 @@ export class Messaging {
             };
 
             await this.processAirDataUseCase.execute(input);
-        });
-    }
+        }
+    );
+  }
 
-    public async publish(topic: string, message: string): Promise<void> {
-        await this.client.publish(topic, message);
-    }
+  public async publish(topic: string, message: string): Promise<void> {
+    await this.client.publish(topic, message);
+  }
 
-    private getDeviceId(topic: string): string {
-        return topic.split('/')[1] || '';
-    }
+  private getDeviceId(topic: string): string {
+    return topic.split('/')[1] || '';
+  }
 }
-
