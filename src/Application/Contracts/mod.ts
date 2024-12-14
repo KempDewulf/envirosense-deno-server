79ebbaa7c726f1b6e7387adfeef4f47a03e488a8
layton <layton.berth@student.howest.be>
2024-12-14 18:30:39 +0100

    Add building-related input and output types to mod.ts for enhanced functionality

--- src/Application/Contracts/mod.ts
+++ src/Application/Contracts/mod.ts
@@ -16,6 +16,17 @@ export type { RoomRepository } from "./Ports/Repositories/Room/RoomRepository.ts
 export type { ShowBuildingsInput } from "./Building/ShowBuildingsInput.ts";
 export type { ShowBuildingsOutput } from "./Building/ShowBuildingsOutput.ts";
 
+export type { CreateBuildingInput } from "./Building/CreateBuildingInput.ts";
+export type { CreateBuildingOutput } from "./Building/CreateBuildingOutput.ts";
+
+export type { ShowBuildingByDocumentIdInput } from "./Building/ShowBuildingByDocumentIdInput.ts";
+export type { ShowBuildingByDocumentIdOutput } from "./Building/ShowBuildingByDocumentIdOutput.ts";
+
+export type { UpdateBuildingInput } from "./Building/UpdateBuildingInput.ts";
+export type { UpdateBuildingOutput } from "./Building/UpdateBuildingOutput.ts";
+
+export type { DeleteBuildingInput } from "./Building/DeleteBuildingInput.ts";
+
 export type {
     BuildingQueryAllDto,
     BuildingQueryRepository,
