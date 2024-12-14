edd4bcbad2fafcbda4dc85b049f71bfffc76a6cc
layton <layton.berth@student.howest.be>
2024-12-14 18:48:29 +0100

    Add UpdateBuilding use case to handle building updates

---
+++ src/Application/Building/UpdateBuilding.ts
@@ -0,0 +1,50 @@
+import {
+    UpdateBuildingInput,
+    UpdateBuildingOutput,
+    OutputPort,
+    BuildingRepository,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export class UpdateBuilding implements UseCase<UpdateBuildingInput> {
+    private readonly _outputPort: OutputPort<UpdateBuildingOutput>;
+    private readonly _buildingRepository: BuildingRepository;
+
+    constructor(
+        outputPort: OutputPort<UpdateBuildingOutput>,
+        buildingRepository: BuildingRepository,
+    ) {
+        this._outputPort = outputPort;
+        this._buildingRepository = buildingRepository;
+    }
+
+    public async execute(input: UpdateBuildingInput): Promise<void> {
+        const buildingOptional = await this._buildingRepository.find(input.buildingDocumentId);
+        const building = buildingOptional.orElseThrow(() =>
+            new Error(`Building with ID ${input.buildingDocumentId} not found.`)
+        );
+
+        if (input.name !== undefined) {
+            building.updateName(input.name);
+        }
+
+        if (input.icon !== undefined) {
+            building.updateAddress(input.address);
+        }
+
+        if(input.rooms !== undefined) {
+            building.updateRooms(input.rooms);
+        }
+
+        await this._buildingRepository.update(building);
+
+        const output: UpdateBuildingOutput = {
+            id: building.id,
+            name: building.name,
+            address: building.address,
+            rooms: building.rooms,
+        };
+
+        this._outputPort.present(output);
+    }
+}
