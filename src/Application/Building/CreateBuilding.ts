c5f3db33907a28fea4eae7a2f106e49c003b5cb5
layton <layton.berth@student.howest.be>
2024-12-14 18:48:16 +0100

    Add CreateBuilding use case to handle building creation logic

---
+++ src/Application/Building/CreateBuilding.ts
@@ -0,0 +1,35 @@
+import {
+    Building,
+} from 'EnviroSense/Domain/mod.ts';
+import {
+    CreateBuildingInput,
+    CreateBuildingOutput,
+    OutputPort,
+    BuildingRepository,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export class CreateBuilding implements UseCase<CreateBuildingInput> {
+    private readonly _outputPort: OutputPort<CreateBuildingOutput>;
+    private readonly _buildingRepository: BuildingRepository;
+
+    constructor(
+        outputPort: OutputPort<CreateBuildingOutput>,
+        buildingRepository: BuildingRepository,
+    ) {
+        this._outputPort = outputPort;
+        this._buildingRepository = buildingRepository;
+    }
+
+    public async execute(input: CreateBuildingInput): Promise<void> {
+        const building = Building.create(
+            '',
+            input.name,
+            input.address
+        );
+
+        await this._buildingRepository.save(building);
+        this._outputPort.present({ id: building.id.toString() });
+    }
+}
+
