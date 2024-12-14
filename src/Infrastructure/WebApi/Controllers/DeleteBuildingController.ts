642b4368347ec50bba4a0ea661ba1635b754762e
layton <layton.berth@student.howest.be>
2024-12-14 18:33:51 +0100

    Add DeleteBuildingController to handle building deletion requests

---
+++ src/Infrastructure/WebApi/Controllers/DeleteBuildingController.ts
@@ -0,0 +1,25 @@
+import { DeleteBuildingInput, UseCase } from 'EnviroSense/Application/Contracts/mod.ts';
+import { Controller } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+
+export interface DeleteBuildingRequest {
+    buildingDocumentId: string;
+}
+
+export class DeleteBuildingController implements Controller<DeleteBuildingRequest> {
+    private readonly _useCase: UseCase<DeleteBuildingInput>;
+
+    constructor(useCase: UseCase<DeleteBuildingInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(request: DeleteBuildingRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: DeleteBuildingRequest): DeleteBuildingInput {
+        return {
+            buildingDocumentId: request.buildingDocumentId,
+        };
+    }
+}
