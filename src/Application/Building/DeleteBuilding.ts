86f47c3973ace09a12838507f00825807a0c4e79
layton <layton.berth@student.howest.be>
2024-12-14 18:48:19 +0100

    Add DeleteBuilding use case to handle building deletion logic

---
+++ src/Application/Building/DeleteBuilding.ts
@@ -0,0 +1,23 @@
+import {
+    DeleteBuildingInput,
+    BuildingRepository,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export class DeleteBuilding implements UseCase<DeleteBuildingInput> {
+    private readonly _buildingRepository: BuildingRepository;
+
+    constructor(
+        buildingRepository: BuildingRepository,
+    ) {
+        this._buildingRepository = buildingRepository;
+    }
+
+    public async execute(input: DeleteBuildingInput): Promise<void> {
+        const roomType = (await this._buildingRepository.find(input.buildingDocumentId))
+            .orElseThrow(() => new Error(`Building with ID ${input.buildingDocumentId} not found.`));
+
+        await this._buildingRepository.deleteEntity(roomType);
+    }
+}
+
