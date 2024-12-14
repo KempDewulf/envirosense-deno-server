83d6d491b5ebfdf0ecbc783c538e5601b28c26f5
Layton Berth <laytonbrth@gmail.com>
2024-12-14 01:37:55 +0100

    Remove TestEndpoint and update module exports accordingly

--- src/Infrastructure/WebApi/mod.ts
+++ src/Infrastructure/WebApi/mod.ts
@@ -3,7 +3,6 @@ export { WebApiModule } from "EnviroSense/Infrastructure/WebApi/WebApiModule.ts"
 export { endpoints } from "EnviroSense/Infrastructure/WebApi/Endpoints.ts";
 export { errorHandlingMiddleware } from "EnviroSense/Infrastructure/WebApi/Middleware/ErrorHandlingMiddleware.ts";
 export type { Endpoint } from "EnviroSense/Infrastructure/WebApi/Shared/Endpoint.ts";
-export { TestEndpoint } from "EnviroSense/Infrastructure/WebApi/Endpoints/TestEndpoint.ts";
 
 //OpenAPI
 export { ShowOpenApiEndpoint } from "EnviroSense/Infrastructure/WebApi/Endpoints/ShowOpenApiEndpoint.ts";
