45bbdd3742a343973ddf3e2b0b29bc6508a56087
layton <layton.berth@student.howest.be>
2024-12-14 18:49:10 +0100

    Refactor RoomTypeStrapiQueryRepository to improve code formatting and readability

--- src/Infrastructure/Persistence/Repositories/Strapi/RoomType/RoomTypeStrapiQueryRepository.ts
+++ src/Infrastructure/Persistence/Repositories/Strapi/RoomType/RoomTypeStrapiQueryRepository.ts
@@ -2,8 +2,8 @@ import {
     RoomTypeQueryRepository,
     RoomTypeQueryDto,
 } from "EnviroSense/Application/Contracts/mod.ts";
-import { StrapiQueryRepository } from "../../../Shared/StrapiQueryRepository.ts";import { Optional } from 'EnviroSense/Domain/mod.ts';
-
+import { StrapiQueryRepository } from "../../../Shared/StrapiQueryRepository.ts";
+import { Optional } from "EnviroSense/Domain/mod.ts";
 
 export class RoomTypeStrapiQueryRepository
     extends StrapiQueryRepository
@@ -20,7 +20,9 @@ export class RoomTypeStrapiQueryRepository
         return roomTypes;
     }
 
-    async find(roomTypeDocumentId: string): Promise<Optional<RoomTypeQueryDto>> {
+    async find(
+        roomTypeDocumentId: string
+    ): Promise<Optional<RoomTypeQueryDto>> {
         const endpoint = `room-types/${roomTypeDocumentId.toString()}`;
         const params: Record<string, string> = {};
 
