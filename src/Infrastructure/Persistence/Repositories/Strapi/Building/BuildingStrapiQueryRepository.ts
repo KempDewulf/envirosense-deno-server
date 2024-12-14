6307be4ec8ae10b4acb228fd59d9cc2fd5088ca5
layton <layton.berth@student.howest.be>
2024-12-14 18:49:03 +0100

    Refactor BuildingStrapiQueryRepository to use BuildingQueryDto and add find method for building retrieval by document ID

--- src/Infrastructure/Persistence/Repositories/Strapi/Building/BuildingStrapiQueryRepository.ts
+++ src/Infrastructure/Persistence/Repositories/Strapi/Building/BuildingStrapiQueryRepository.ts
@@ -1,29 +1,45 @@
 import {
     BuildingQueryRepository,
-    BuildingQueryAllDto,
+    BuildingQueryDto,
 } from "EnviroSense/Application/Contracts/mod.ts";
 import { StrapiQueryRepository } from "../../../Shared/StrapiQueryRepository.ts";
+import { Optional } from 'EnviroSense/Domain/mod.ts';
 
 export class BuildingStrapiQueryRepository
     extends StrapiQueryRepository
     implements BuildingQueryRepository
 {
-    async all(name: string): Promise<BuildingQueryAllDto[]> {
-        const endpoint = 'buildings';
-        const params = name ? { 'filters[name][$contains]': name, 'populate': '*' } : undefined;
+    async all(name: string): Promise<BuildingQueryDto[]> {
+        const endpoint = "buildings";
+        const params = name
+            ? { "filters[name][$contains]": name, populate: "*" }
+            : undefined;
         const response = await this.get<any>(endpoint, params);
 
         const buildings = response.data.map((item: any) => this.mapToDto(item));
         return buildings;
     }
 
-    private mapToDto(item: any): BuildingQueryAllDto {
+    async find(
+        buildingDocumentId: string
+    ): Promise<Optional<BuildingQueryDto>> {
+        const endpoint = `buildings/${buildingDocumentId.toString()}`;
+        const params: Record<string, string> = {};
+
+        const response = await this.get<any>(endpoint, params);
+
+        const building = this.mapToDto(response.data);
+
+        return Optional.of<BuildingQueryDto>(building);
+    }
+
+    private mapToDto(item: any): BuildingQueryDto {
         return {
             id: item.id,
             documentId: item.documentId,
             name: item.name,
             address: item.address,
-            rooms: item.rooms
+            rooms: item.rooms,
         };
     }
 }
