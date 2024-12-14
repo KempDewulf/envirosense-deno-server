b60c57422eaf97eb7b3126c163f23a8472cc9496
layton <layton.berth@student.howest.be>
2024-12-14 18:48:48 +0100

    Add ShowBuildingByDocumentIdOutput interface for building retrieval output structure

---
+++ src/Application/Contracts/Building/ShowBuildingByDocumentIdOutput.ts
@@ -0,0 +1,9 @@
+import { Room } from "EnviroSense/Domain/mod.ts";
+
+export interface ShowBuildingByDocumentIdOutput {
+    id: string;
+    documentId: string;
+    name: string;
+    address: string;
+    rooms: Room[];
+}
