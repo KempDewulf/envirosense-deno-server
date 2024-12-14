78757de5995bda2ba9a1d2a8a116b1db244a896f
Layton Berth <laytonbrth@gmail.com>
2024-12-14 01:39:55 +0100

    Update ShowOpenApiEndpoint to adjust OpenAPI YAML file path

--- src/Infrastructure/WebApi/Endpoints/ShowOpenApiEndpoint.ts
+++ src/Infrastructure/WebApi/Endpoints/ShowOpenApiEndpoint.ts
@@ -16,7 +16,7 @@ export class ShowOpenApiEndpoint implements Endpoint {
                 <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
                 <script>
                     const ui = SwaggerUIBundle({
-                        url: './openapi.yml',
+                        url: '../../../../openapi.yml',
                         dom_id: '#swagger-ui',
                         presets: [
                             SwaggerUIBundle.presets.apis,
