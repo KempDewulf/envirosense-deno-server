66ed8c050351c1d3d7e7cc9da9e1e3c1472b69fe
layton <layton.berth@student.howest.be>
2024-12-16 15:26:23 +0100

    Add RemoveDeviceFromRoom use case for managing device removal from rooms

---
+++ src/Application/Room/RemoveDeviceFromRoom.ts
@@ -0,0 +1,54 @@
+import {
+    DeviceRepository,
+    UseCase,
+    RemoveDeviceFromRoomInput,
+    RoomRepository,
+} from "EnviroSense/Application/Contracts/mod.ts";
+import { DeviceOperation } from 'EnviroSense/Infrastructure/Persistence/Repositories/Strapi/Room/RoomStrapiRepository.ts';
+
+export class RemoveDeviceFromRoom
+    implements UseCase<RemoveDeviceFromRoomInput>
+{
+    private readonly _roomRepository: RoomRepository;
+    private readonly _deviceRepository: DeviceRepository;
+
+    constructor(
+        roomRepository: RoomRepository,
+        deviceRepository: DeviceRepository,
+    ) {
+        this._roomRepository = roomRepository;
+        this._deviceRepository = deviceRepository;
+    }
+
+    async execute(input: RemoveDeviceFromRoomInput): Promise<void> {
+        const roomOptional = await this._roomRepository.find(
+            input.roomDocumentId
+        );
+
+        const room = roomOptional.orElseThrow(
+            () =>
+                new Error(
+                    `Building with ID ${input.roomDocumentId} not found.`
+                )
+        );
+
+        const deviceOptional = await this._deviceRepository.find(
+            input.deviceDocumentId
+        );
+
+        const device = deviceOptional.orElseThrow(
+            () =>
+                new Error(
+                    `Device with documentId ${input.deviceDocumentId} not found.`
+                )
+        );
+
+        room.removeDevice(device.id);
+
+        await this._roomRepository.manageDevices(
+            room.id,
+            [input.deviceDocumentId],
+            DeviceOperation.REMOVE
+        );
+    }
+}
