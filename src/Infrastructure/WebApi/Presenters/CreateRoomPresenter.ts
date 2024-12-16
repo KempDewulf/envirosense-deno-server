25e9dddf3951c1f0980ad548db0b142e2e5b8599
layton <layton.berth@student.howest.be>
2024-12-16 14:21:30 +0100

    Add CreateRoomPresenter to handle room creation output presentation

---
+++ src/Infrastructure/WebApi/Presenters/CreateRoomPresenter.ts
@@ -0,0 +1,25 @@
+import { RequestResponseDevice } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import { CreateRoomOutput, OutputPort } from 'EnviroSense/Application/Contracts/mod.ts';
+
+export interface CreateRoomPresentedData {
+    url: string;
+}
+
+export class CreateRoomPresenter implements OutputPort<CreateRoomOutput> {
+    private readonly _device: RequestResponseDevice<CreateRoomPresentedData>;
+
+    constructor(device: RequestResponseDevice<CreateRoomPresentedData>) {
+        this._device = device;
+    }
+
+    present(data: CreateRoomOutput): void {
+        const presentedData = this.mapToPresentedData(data);
+        this._device.update(presentedData);
+    }
+
+    protected mapToPresentedData(data: CreateRoomOutput): CreateRoomPresentedData {
+        return {
+            url: `/rooms/${data.id}`,
+        };
+    }
+}
