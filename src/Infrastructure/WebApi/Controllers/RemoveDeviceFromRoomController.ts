638ca9f394fcb2688ae07cd48fd9e3345409d6f4
layton <layton.berth@student.howest.be>
2024-12-16 14:50:55 +0100

    Add RemoveDeviceFromRoomController to handle device removal from a room

---
+++ src/Infrastructure/WebApi/Controllers/RemoveDeviceFromRoomController.ts
@@ -0,0 +1,33 @@
+import { Controller } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import {
+    RemoveDeviceFromRoomInput,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export interface RemoveDeviceFromRoomRequest {
+    roomDocumentId: string;
+    deviceDocumentId: string;
+}
+
+export class RemoveDeviceFromRoomController
+    implements Controller<RemoveDeviceFromRoomRequest> {
+    private readonly _useCase: UseCase<RemoveDeviceFromRoomInput>;
+
+    constructor(useCase: UseCase<RemoveDeviceFromRoomInput>) {
+        this._useCase = useCase;
+    }
+
+    handle(request: RemoveDeviceFromRoomRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        return this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(
+        request: RemoveDeviceFromRoomRequest,
+    ): RemoveDeviceFromRoomInput {
+        return {
+            roomDocumentId: request.roomDocumentId,
+            deviceDocumentId: request.deviceDocumentId,
+        };
+    }
+}
