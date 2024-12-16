dd831187d6000c40142ef7e283a0a9b134982433
layton <layton.berth@student.howest.be>
2024-12-16 01:22:52 +0100

    Update StrapiQueryRepository base URL and enhance room and building endpoints

--- src/Infrastructure/WebApi/Endpoints.ts
+++ src/Infrastructure/WebApi/Endpoints.ts
@@ -26,19 +26,26 @@ export function endpoints(): Router {
     const router = new Router();
 
     router.get('/', use(new ShowOpenApiEndpoint()));
+
     router.get('/rooms', use(new ShowRoomsEndpoint()));
+    router.get('/rooms/:roomDocumentId', use(new ShowRoomByDocumentIdEndpoint()));
+    router.post('/rooms', use(new CreateRoomEndpoint()));
+    router.post('/rooms/:roomDocumentId/devices', use(new AddDeviceToRoomEndpoint()));
+    router.delete('/rooms/:roomDocumentId', use(new DeleteRoomEndpoint()));
+    router.delete('/rooms/:roomDocumentId/devices/:deviceDocumentId', use(new RemoveDeviceFromRoomEndpoint()));
+    router.put('/rooms/:roomDocumentId', use(new UpdateRoomEndpoint()));
 
     router.get('/buildings', use(new ShowBuildingsEndpoint()));
-    router.post('/buildings', use(new CreateBuildingEndpoint()));
     router.get('/buildings/:buildingDocumentId', use(new ShowBuildingByDocumentIdEndpoint()));
-    router.delete('/buildings/:buildingDocumentId', use(new DeleteBuildingEndpoint()));
-    router.put('/buildings/:buildingDocumentId', use(new UpdateBuildingEndpoint()));
+    router.post('/buildings', use(new CreateBuildingEndpoint()));
     router.post('/buildings/:buildingDocumentId/rooms', use(new AddRoomToBuildingEndpoint()));
+    router.delete('/buildings/:buildingDocumentId', use(new DeleteBuildingEndpoint()));
     router.delete('/buildings/:buildingDocumentId/rooms/:roomDocumentId', use(new RemoveRoomFromBuildingEndpoint()));
+    router.put('/buildings/:buildingDocumentId', use(new UpdateBuildingEndpoint()));
 
     router.get('/room-types', use(new ShowRoomTypesEndpoint()));
-    router.post('/room-types', use(new CreateRoomTypeEndpoint()));
     router.get('/room-types/:roomTypeDocumentId', use(new ShowRoomTypeByDocumentIdEndpoint()));
+    router.post('/room-types', use(new CreateRoomTypeEndpoint()));
     router.delete('/room-types/:roomTypeDocumentId', use(new DeleteRoomTypeEndpoint()));
     router.put('/room-types/:roomTypeDocumentId', use(new UpdateRoomTypeEndpoint()));
 
