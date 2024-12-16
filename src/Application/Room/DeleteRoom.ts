a3b091498eec1f3543b5ac4b9c2f6f7e3ccd6390
layton <layton.berth@student.howest.be>
2024-12-16 15:26:20 +0100

    Add DeleteRoom use case for handling room deletion logic

---
+++ src/Application/Room/DeleteRoom.ts
@@ -0,0 +1,23 @@
+import {
+    DeleteRoomInput,
+    RoomRepository,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export class DeleteRoom implements UseCase<DeleteRoomInput> {
+    private readonly _roomRepository: RoomRepository;
+
+    constructor(
+        roomRepository: RoomRepository,
+    ) {
+        this._roomRepository = roomRepository;
+    }
+
+    public async execute(input: DeleteRoomInput): Promise<void> {
+        const roomType = (await this._roomRepository.find(input.roomDocumentId))
+            .orElseThrow(() => new Error(`Room with ID ${input.roomDocumentId} not found.`));
+
+        await this._roomRepository.deleteEntity(roomType);
+    }
+}
+
