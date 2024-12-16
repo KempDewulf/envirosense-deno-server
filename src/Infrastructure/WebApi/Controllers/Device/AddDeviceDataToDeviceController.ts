b33312a31b9945a5d76f4e75d2f9a20fc00829e2
layton <layton.berth@student.howest.be>
2024-12-16 20:29:43 +0100

    Add AddDeviceDataToDeviceController for handling device data addition requests

---
+++ src/Infrastructure/WebApi/Controllers/Device/AddDeviceDataToDeviceController.ts
@@ -0,0 +1,33 @@
+import { Controller } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import {
+    AddDeviceDataToDeviceInput,
+    UseCase,
+} from 'EnviroSense/Application/Contracts/mod.ts';
+
+export interface AddDeviceDataToDeviceRequest {
+    roomDocumentId: string;
+    deviceData: string[];
+}
+
+export class AddDeviceDataToDeviceController
+    implements Controller<AddDeviceDataToDeviceRequest> {
+    private readonly _useCase: UseCase<AddDeviceDataToDeviceInput>;
+
+    constructor(useCase: UseCase<AddDeviceDataToDeviceInput>) {
+        this._useCase = useCase;
+    }
+
+    handle(request: AddDeviceDataToDeviceRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        return this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(
+        request: AddDeviceDataToDeviceRequest,
+    ): AddDeviceDataToDeviceInput {
+        return {
+            roomDocumentId: request.roomDocumentId,
+            deviceData: request.deviceData
+        };
+    }
+}
