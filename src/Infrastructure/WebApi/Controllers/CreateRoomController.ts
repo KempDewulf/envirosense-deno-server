a609a2234f4e38e81ea6e643edecdbecb4ec710d
layton <layton.berth@student.howest.be>
2024-12-16 14:50:49 +0100

    Add CreateRoomController to handle room creation requests

---
+++ src/Infrastructure/WebApi/Controllers/CreateRoomController.ts
@@ -0,0 +1,29 @@
+import { CreateRoomInput, UseCase } from 'EnviroSense/Application/Contracts/mod.ts';
+import { Controller } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+
+export interface CreateRoomRequest {
+    name: string;
+    buildingDocumentId: string;
+    roomTypeDocumentId: string;
+}
+
+export class CreateRoomTypeController implements Controller<CreateRoomRequest> {
+    private readonly _useCase: UseCase<CreateRoomInput>;
+
+    constructor(useCase: UseCase<CreateRoomInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(request: CreateRoomRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: CreateRoomRequest): CreateRoomInput {
+        return {
+            name: request.name,
+            buildingDocumentId: request.buildingDocumentId,
+            roomTypeDocumentId: request.roomTypeDocumentId
+        };
+    }
+}
