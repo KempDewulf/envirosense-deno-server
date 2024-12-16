965ac79248bb0f113171665c27625ff0cb76b086
layton <layton.berth@student.howest.be>
2024-12-16 15:26:34 +0100

    Add UpdateRoom use case for updating room details and handling validation

---
+++ src/Application/Room/UpdateRoom.ts
@@ -0,0 +1,43 @@
+import {
+    UpdateRoomInput,
+    UpdateRoomOutput,
+    OutputPort,
+    RoomRepository,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export class UpdateRoom implements UseCase<UpdateRoomInput> {
+    private readonly _outputPort: OutputPort<UpdateRoomOutput>;
+    private readonly _roomRepository: RoomRepository;
+
+    constructor(
+        outputPort: OutputPort<UpdateRoomOutput>,
+        roomRepository: RoomRepository
+    ) {
+        this._outputPort = outputPort;
+        this._roomRepository = roomRepository;
+    }
+
+    public async execute(input: UpdateRoomInput): Promise<void> {
+        const roomOptional = await this._roomRepository.find(
+            input.roomDocumentId
+        );
+        const room = roomOptional.orElseThrow(
+            () => new Error(`Room with ID ${input.roomDocumentId} not found.`)
+        );
+
+        if (input.name !== undefined) {
+            room.updateName(input.name);
+        }
+
+        await this._roomRepository.update(room);
+
+        const output: UpdateRoomOutput = {
+            id: room.id,
+            documentId: room.id,
+            name: room.name,
+        };
+
+        this._outputPort.present(output);
+    }
+}
