d690da227918306a2f67fe186821701942fe8b7e
layton <layton.berth@student.howest.be>
2024-12-16 20:29:46 +0100

    Add CreateDeviceController for handling device creation requests

---
+++ src/Infrastructure/WebApi/Controllers/Device/CreateDeviceController.ts
@@ -0,0 +1,30 @@
+import { CreateDeviceInput, UseCase } from 'EnviroSense/Application/Contracts/mod.ts';
+import { Controller } from 'EnviroSense/Infrastructure/Shared/mod.ts';
+import { DeviceData, Room } from 'EnviroSense/Domain/mod.ts';
+
+export interface CreateDeviceRequest {
+    identifier: string;
+    room: Room;
+    deviceData: DeviceData[];
+}
+
+export class CreateDeviceController implements Controller<CreateDeviceRequest> {
+    private readonly _useCase: UseCase<CreateDeviceInput>;
+
+    constructor(useCase: UseCase<CreateDeviceInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(request: CreateDeviceRequest): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: CreateDeviceRequest): CreateDeviceInput {
+        return {
+            identifier: request.identifier,
+            room: request.room,
+            deviceData: request.deviceData,
+        };
+    }
+}
