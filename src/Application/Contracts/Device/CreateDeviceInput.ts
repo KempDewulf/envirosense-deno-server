4a3ddd911e704b4af6aa71845ecd5ff53fb63d8c
layton <layton.berth@student.howest.be>
2024-12-16 21:00:00 +0100

    Update CreateDeviceInput to replace room and device_data with roomDocumentId

--- src/Application/Contracts/Device/CreateDeviceInput.ts
+++ src/Application/Contracts/Device/CreateDeviceInput.ts
@@ -1,7 +1,4 @@
-import { DeviceData, Room } from 'EnviroSense/Domain/mod.ts';
-
 export interface CreateDeviceInput {
     identifier: string;
-    room: Room;
-    device_data: DeviceData[];
+    roomDocumentId: string;
 }
