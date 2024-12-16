16eb13611e65f2ebeeb449fb69d5875ac8f516f1
layton <layton.berth@student.howest.be>
2024-12-16 15:25:38 +0100

    Rename RoomQueryAllDto to RoomQueryDto and update RoomQueryRepository interface to include find method for retrieving room details by document ID

--- src/Application/Contracts/Ports/Repositories/Room/RoomQueryRepository.ts
+++ src/Application/Contracts/Ports/Repositories/Room/RoomQueryRepository.ts
@@ -1,14 +1,15 @@
-import { Building, Device, RoomType } from 'EnviroSense/Domain/mod.ts';
+import { Building, Device, Optional, RoomType } from "EnviroSense/Domain/mod.ts";
 
-export interface RoomQueryAllDto {
+export interface RoomQueryDto {
     id: string;
     documentId: string;
     name: string;
     building: Building;
-    'room-type': RoomType;
+    "room-type": RoomType;
     devices: Device[];
 }
 
 export interface RoomQueryRepository {
-    all(name: string): Promise<RoomQueryAllDto[]>;
+    all(name: string): Promise<RoomQueryDto[]>;
+    find(roomDocumentId: string): Promise<Optional<RoomQueryDto>>;
 }
