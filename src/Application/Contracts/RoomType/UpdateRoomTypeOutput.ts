6fb1490084f12c243cdae97c18509e31853c1049
layton <layton.berth@student.howest.be>
2024-12-16 15:26:12 +0100

    Remove rooms property from UpdateRoomTypeOutput interface

--- src/Application/Contracts/RoomType/UpdateRoomTypeOutput.ts
+++ src/Application/Contracts/RoomType/UpdateRoomTypeOutput.ts
@@ -1,9 +1,6 @@
-import { Room } from 'EnviroSense/Domain/mod.ts';
-
 export interface UpdateRoomTypeOutput {
     id: string;
     documentId: string;
     name: string;
     icon: string;
-    rooms: Room[];
 }
