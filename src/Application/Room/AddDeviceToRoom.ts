13c9611332ac24dc9d8811ed9a98852650f20453
layton <layton.berth@student.howest.be>
2024-12-16 15:26:15 +0100

    Add AddDeviceToRoom use case for managing device associations in rooms

---
+++ src/Application/Room/AddDeviceToRoom.ts
@@ -0,0 +1,54 @@
+import {
+    UseCase,
+    AddDeviceToRoomInput,
+    RoomRepository,
+    DeviceRepository,
+} from "EnviroSense/Application/Contracts/mod.ts";
+import { DeviceOperation } from 'EnviroSense/Infrastructure/Persistence/Repositories/Strapi/Room/RoomStrapiRepository.ts';
+
+export class AddDeviceToRoom implements UseCase<AddDeviceToRoomInput> {
+    private readonly _roomRepository: RoomRepository;
+    private readonly _deviceRepository: DeviceRepository;
+
+    constructor(
+        roomRepository: RoomRepository,
+        deviceRepository: DeviceRepository,
+    ) {
+        this._roomRepository = roomRepository;
+        this._deviceRepository = deviceRepository;
+    }
+
+    async execute(input: AddDeviceToRoomInput): Promise<void> {
+        const roomOptional = await this._roomRepository.find(
+            input.roomDocumentId
+        );
+
+        const room = roomOptional.orElseThrow(
+            () =>
+                new Error(
+                    `Room with ID ${input.roomDocumentId} not found.`
+                )
+        );
+
+        const deviceDocumentIdsToConnect: string[] = [];
+
+        for (const deviceDocumentId of input.devices) {
+            const deviceOptional = await this._deviceRepository.find(
+                deviceDocumentId
+            );
+
+            const device = deviceOptional.orElseThrow(
+                () =>
+                    new Error(
+                        `Device with documentId ${deviceDocumentId} not found.`
+                    )
+            );
+
+            room.addDevice(device);
+
+            deviceDocumentIdsToConnect.push(device.id);
+        }
+
+        await this._roomRepository.manageDevices(room.id, deviceDocumentIdsToConnect, DeviceOperation.ADD);
+    }
+}
