596c0e354b04a211029eaf61fca185bdb4ea4b62
layton <layton.berth@student.howest.be>
2024-12-16 15:26:26 +0100

    Add ShowRoomByDocumentId use case for retrieving room details by document ID

---
+++ src/Application/Room/ShowRoomByDocumentId.ts
@@ -0,0 +1,49 @@
+import {
+    OutputPort,
+    UseCase,
+    ShowRoomByDocumentIdInput,
+    ShowRoomByDocumentIdOutput,
+    RoomQueryDto,
+    RoomQueryRepository,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export class ShowRoomByDocumentId
+    implements UseCase<ShowRoomByDocumentIdInput>
+{
+    private readonly _outputPort: OutputPort<ShowRoomByDocumentIdOutput>;
+    private readonly _roomRepository: RoomQueryRepository;
+
+    constructor(
+        outputPort: OutputPort<ShowRoomByDocumentIdOutput>,
+        roomRepository: RoomQueryRepository
+    ) {
+        this._outputPort = outputPort;
+        this._roomRepository = roomRepository;
+    }
+
+    public async execute(input: ShowRoomByDocumentIdInput): Promise<void> {
+        const roomOptional = await this._roomRepository.find(
+            input.roomDocumentId
+        );
+
+        const roomDto = roomOptional.orElseThrow(() =>
+            new Error(`Room with ID ${input.roomDocumentId} not found.`)
+        );
+
+        const room = this.mapDtoToOutput(roomDto);
+        this._outputPort.present(room);
+    }
+
+    private mapDtoToOutput(
+        dto: RoomQueryDto
+    ): ShowRoomByDocumentIdOutput {
+        return {
+            id: dto.id,
+            documentId: dto.documentId,
+            name: dto.name,
+            building: dto.building,
+            "room-type": dto['room-type'],
+            devices: dto.devices
+        };
+    }
+}
