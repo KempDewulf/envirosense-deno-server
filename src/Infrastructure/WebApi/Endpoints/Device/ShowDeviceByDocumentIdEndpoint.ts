1e482c6f6e41cb4f6f48f398343830369f788274
layton <layton.berth@student.howest.be>
2024-12-16 20:22:27 +0100

    Add ShowDeviceByDocumentIdEndpoint for retrieving device details by document ID

---
+++ src/Infrastructure/WebApi/Endpoints/Device/ShowDeviceByDocumentIdEndpoint.ts
@@ -0,0 +1,43 @@
+import { RouterContext } from "@oak/oak";
+import {
+    Endpoint,
+    ShowDeviceByDocumentIdController,
+    ShowDeviceByDocumentIdPresentedData,
+    ShowDeviceByDocumentIdPresenter,
+    ShowDeviceByDocumentIdRequest,
+} from "EnviroSense/Infrastructure/WebApi/mod.ts";
+import { RequestResponse } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import { DeviceStrapiQueryRepository } from "EnviroSense/Infrastructure/Persistence/mod.ts";
+import { ShowDeviceByDocumentId } from "EnviroSense/Application/mod.ts";
+
+export class ShowDeviceByDocumentIdEndpoint implements Endpoint {
+    async handle(context: RouterContext<string>): Promise<void> {
+        const outputDevice = new RequestResponse<ShowDeviceByDocumentIdPresentedData>();
+        const presenter = new ShowDeviceByDocumentIdPresenter(outputDevice);
+
+        const repository = new DeviceStrapiQueryRepository();
+
+        const useCase = new ShowDeviceByDocumentId(presenter, repository);
+
+        const controller = new ShowDeviceByDocumentIdController(useCase);
+        const request = this.buildRequest(context);
+        await controller.handle(request);
+
+        context.response.headers.set("Content-Type", "application/json");
+        context.response.body = outputDevice.response;
+
+        return Promise.resolve();
+    }
+
+    static create(): Endpoint {
+        return new ShowDeviceByDocumentIdEndpoint();
+    }
+
+    private buildRequest(
+        context: RouterContext<string>
+    ): ShowDeviceByDocumentIdRequest {
+        const deviceDocumentId = context.params.deviceDocumentId || "";
+
+        return { deviceDocumentId } as ShowDeviceByDocumentIdRequest;
+    }
+}
