46ae1856210553a595ed0fff407b606178e9362b
layton <layton.berth@student.howest.be>
2024-12-17 09:39:32 +0100

    Add ShowDeviceDataByDocumentIdController to handle device data requests

---
+++ src/Infrastructure/WebApi/Controllers/DeviceData/ShowDeviceDataByDocumentIdController.ts
@@ -0,0 +1,30 @@
+import { Controller } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import {
+    ShowDeviceDataByDocumentIdInput,
+    UseCase,
+} from "EnviroSense/Application/Contracts/mod.ts";
+
+export interface ShowDeviceDataByDocumentIdRequest {
+    deviceDataDocumentId: string;
+}
+
+export class ShowDeviceDataByDocumentIdController
+    implements Controller<ShowDeviceDataByDocumentIdRequest>
+{
+    private readonly _useCase: UseCase<ShowDeviceDataByDocumentIdInput>;
+
+    constructor(useCase: UseCase<ShowDeviceDataByDocumentIdInput>) {
+        this._useCase = useCase;
+    }
+
+    public async handle(
+        request: ShowDeviceDataByDocumentIdRequest
+    ): Promise<void> {
+        const useCaseInput = this.mapToUseCaseInput(request);
+        await this._useCase.execute(useCaseInput);
+    }
+
+    protected mapToUseCaseInput(request: ShowDeviceDataByDocumentIdRequest): ShowDeviceDataByDocumentIdInput {
+        return { deviceDataDocumentId: request.deviceDataDocumentId };
+    }
+}
