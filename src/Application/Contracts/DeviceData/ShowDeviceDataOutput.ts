e1b44372054c69844d038ccd7fec9c6edb12e721
layton <layton.berth@student.howest.be>
2024-12-17 09:39:14 +0100

    Update ShowDeviceDataOutput interface to include AirData and change timestamp type to Date

--- src/Application/Contracts/DeviceData/ShowDeviceDataOutput.ts
+++ src/Application/Contracts/DeviceData/ShowDeviceDataOutput.ts
@@ -1,11 +1,9 @@
-import { Device } from 'EnviroSense/Domain/mod.ts';
+import { AirData, Device } from 'EnviroSense/Domain/mod.ts';
 
 export interface ShowDeviceDataOutput {
     id: string;
     documentId: string;
     device: Device;
-    timestamp: number;
-    temperature: number;
-    humidity: number;
-    gas_ppm: number;
+    timestamp: Date;
+    airData: AirData;
 }
