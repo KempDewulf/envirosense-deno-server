d4dc8cac66a734b9f00664ffcac564d77999aca7
Layton Berth <laytonbrth@gmail.com>
2024-12-19 19:34:10 +0100

    Update exposedHeaders in WebApiModule to specify allowed headers

--- src/Infrastructure/WebApi/WebApiModule.ts
+++ src/Infrastructure/WebApi/WebApiModule.ts
@@ -25,7 +25,7 @@ export class WebApiModule implements Module {
 				optionsSuccessStatus: 200,
 				methods: ["*"],
 				allowedHeaders: ["*"],
-				exposedHeaders: ["*"],
+				exposedHeaders: ["Location", "Content-Type", "*"],
 			},
 		));
 
