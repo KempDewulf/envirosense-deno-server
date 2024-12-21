7d8f6cf8e5927d17ebed3e6e79bfe25986f6e1f2
Layton Berth <laytonbrth@gmail.com>
2024-12-21 01:50:39 +0100

    Refactor DeleteAllDeviceDeviceDataFromDevicePresenter to DeleteAllDeviceDataFromDevicePresenter

--- src/Infrastructure/WebApi/Presenters/Device/DeleteAllDeviceDataFromDevicePresenter.ts
+++ src/Infrastructure/WebApi/Presenters/Device/DeleteAllDeviceDataFromDevicePresenter.ts
@@ -1,7 +1,7 @@
 import { RequestResponseDevice } from "EnviroSense/Infrastructure/Shared/mod.ts";
 import { OutputPort } from "EnviroSense/Application/Contracts/mod.ts";
 
-export class DeleteAllDeviceDeviceDataFromDevicePresenter implements OutputPort<void> {
+export class DeleteAllDeviceDataFromDevicePresenter implements OutputPort<void> {
 	private readonly _device: RequestResponseDevice<void>;
 
 	constructor(device: RequestResponseDevice<void>) {
