3afd8812d42bb5b7120b7f4881706c5323ee4360
Layton Berth <laytonbrth@gmail.com>
2024-12-21 02:16:42 +0100

    Fix DELETE endpoint for device data by removing trailing slash

--- src/Infrastructure/WebApi/Endpoints.ts
+++ src/Infrastructure/WebApi/Endpoints.ts
@@ -115,7 +115,7 @@ export function endpoints(): Router {
 		"/devices/:deviceDocumentId",
 		use(new DeleteDeviceEndpoint()),
 	);
-	router.delete('/devices/:deviceDocumentId/device-data/', use(new DeleteAllDeviceDataFromDeviceEndpoint()));
+	router.delete('/devices/:deviceDocumentId/device-data', use(new DeleteAllDeviceDataFromDeviceEndpoint()));
 	router.put("/devices/:deviceDocumentId", use(new UpdateDeviceEndpoint()));
 
 	router.get("/device-data", use(new ShowDeviceDataEndpoint()));
