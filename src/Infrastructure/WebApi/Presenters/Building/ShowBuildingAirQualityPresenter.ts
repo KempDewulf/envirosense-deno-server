32585db4c9eb76215210093ac17d14daf8160f04
Layton Berth <laytonbrth@gmail.com>
2024-12-23 17:53:36 +0100

    Add ShowBuildingAirQualityPresenter to format and present air quality data for buildings

---
+++ src/Infrastructure/WebApi/Presenters/Building/ShowBuildingAirQualityPresenter.ts
@@ -0,0 +1,35 @@
+import { RequestResponseDevice } from "EnviroSense/Infrastructure/Shared/mod.ts";
+import { OutputPort, RoomAirQualityOutput, ShowBuildingAirQualityOutput } from "EnviroSense/Application/Contracts/mod.ts";
+
+export type ShowBuildingAirQualityPresentedData = {
+	id: string;
+	enviroScore: number | null;
+	rooms: RoomAirQualityOutput[];
+};
+
+export class ShowBuildingAirQualityPresenter implements OutputPort<ShowBuildingAirQualityOutput> {
+	private readonly _device: RequestResponseDevice<
+		ShowBuildingAirQualityPresentedData
+	>;
+
+	constructor(
+		device: RequestResponseDevice<ShowBuildingAirQualityPresentedData>,
+	) {
+		this._device = device;
+	}
+
+	present(data: ShowBuildingAirQualityOutput): void {
+		const presentedData = this.mapToPresentedData(data);
+		this._device.update(presentedData);
+	}
+
+	protected mapToPresentedData(
+		data: ShowBuildingAirQualityOutput,
+	): ShowBuildingAirQualityPresentedData {
+		return {
+			id: data.id,
+			enviroScore: data.enviroScore,
+            rooms: data.rooms,
+		};
+	}
+}
