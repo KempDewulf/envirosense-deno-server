ec30b7e28d97568f5d1cba58484c3cb296c1f108
layton <layton.berth@student.howest.be>
2024-12-26 20:36:28 +0100

    Refactor RoomType and DeviceData classes to replace 'documentId' with '_documentId' for consistency

--- src/Domain/Entities/Building.ts
+++ src/Domain/Entities/Building.ts
@@ -2,121 +2,121 @@ import { Room } from "EnviroSense/Domain/mod.ts";
 import { DomainException } from "EnviroSense/Domain/Shared/Exceptions/DomainException.ts";
 
 export interface BuildingState {
-	documentId: string;
-	name: string;
-	address: string;
-	rooms?: Room[];
+    documentId: string;
+    name: string;
+    address: string;
+    rooms?: Room[];
 }
 
 export class Building {
-	private readonly _id: string;
-	private _name: string;
-	private _address: string;
-	private _rooms: Room[] = [];
+    private readonly _documentId: string;
+    private _name: string;
+    private _address: string;
+    private _rooms: Room[] = [];
 
-	private constructor(
-		documentId: string,
-		name: string,
-		address: string,
-		rooms?: Room[],
-	) {
-		this.documentId = documentId;
-		this._name = name;
-		this._address = address;
-		this._rooms = rooms ?? [];
-	}
+    private constructor(
+        documentId: string,
+        name: string,
+        address: string,
+        rooms?: Room[]
+    ) {
+        this.documentId = documentId;
+        this._name = name;
+        this._address = address;
+        this._rooms = rooms ?? [];
+    }
 
-	static create(documentId: string, name: string, address: string): Building {
-		const building = new Building(documentId, name, address);
-		building.validateState();
+    static create(documentId: string, name: string, address: string): Building {
+        const building = new Building(documentId, name, address);
+        building.validateState();
 
-		return building;
-	}
+        return building;
+    }
 
-	static load(state: BuildingState): Building {
-		const building = new Building(
-			state.documentId,
-			state.name,
-			state.address,
-			state.rooms,
-		);
-		building.validateState();
+    static load(state: BuildingState): Building {
+        const building = new Building(
+            state.documentId,
+            state.name,
+            state.address,
+            state.rooms
+        );
+        building.validateState();
 
-		return building;
-	}
+        return building;
+    }
 
-	public updateName(name: string): void {
-		if (!name) {
-			throw new DomainException("Name is required.");
-		}
-		this._name = name;
-	}
+    public updateName(name: string): void {
+        if (!name) {
+            throw new DomainException("Name is required.");
+        }
+        this._name = name;
+    }
 
-	public updateAddress(address: string): void {
-		if (!address) {
-			throw new DomainException("Address is required.");
-		}
-		this._address = address;
-	}
+    public updateAddress(address: string): void {
+        if (!address) {
+            throw new DomainException("Address is required.");
+        }
+        this._address = address;
+    }
 
-	public validateState(): void {
-		this.ensureNameIsNotEmpty();
-		this.ensureAddressIsNotEmpty();
-	}
+    public validateState(): void {
+        this.ensureNameIsNotEmpty();
+        this.ensureAddressIsNotEmpty();
+    }
 
-	public addRoom(room: Room): void {
-		this.ensureRoomDoesNotExist(room);
+    public addRoom(room: Room): void {
+        this.ensureRoomDoesNotExist(room);
 
-		this._rooms.push(room);
-	}
+        this._rooms.push(room);
+    }
 
-	public removeRoom(roomDocumentId: string): void {
-		this.ensureRoomExists(roomDocumentId);
+    public removeRoom(roomDocumentId: string): void {
+        this.ensureRoomExists(roomDocumentId);
 
-		this._rooms = this._rooms.filter(
-			(room) => room.documentId !== roomDocumentId,
-		);
-	}
+        this._rooms = this._rooms.filter(
+            (room) => room.documentId !== roomDocumentId
+        );
+    }
 
-	private ensureNameIsNotEmpty(): void {
-		if (!this._name) {
-			throw new DomainException("Name is required.");
-		}
-	}
+    private ensureNameIsNotEmpty(): void {
+        if (!this._name) {
+            throw new DomainException("Name is required.");
+        }
+    }
 
-	private ensureAddressIsNotEmpty(): void {
-		if (!this._address) {
-			throw new DomainException("Address is required.");
-		}
-	}
+    private ensureAddressIsNotEmpty(): void {
+        if (!this._address) {
+            throw new DomainException("Address is required.");
+        }
+    }
 
-	//TODO(@layton): check this later when all room endpoints are made, if we can change this to documentId if needed, if not, great!
-	private ensureRoomDoesNotExist(room: Room): void {
-		if (this._rooms.some((r) => r.documentId === room.documentId)) {
-			throw new DomainException(`Room ${room.documentId} already exists`);
-		}
-	}
+    //TODO(@layton): check this later when all room endpoints are made, if we can change this to documentId if needed, if not, great!
+    private ensureRoomDoesNotExist(room: Room): void {
+        if (this._rooms.some((r) => r.documentId === room.documentId)) {
+            throw new DomainException(`Room ${room.documentId} already exists`);
+        }
+    }
 
-	//TODO(@layton): check this later when all room endpoints are made, if we can change this to documentId if needed, if not, great!
-	public ensureRoomExists(roomDocumentId: string): void {
-		if (!this._rooms.some((room) => room.documentId === roomDocumentId)) {
-			throw new DomainException(`Room does not exist`);
-		}
-	}
+    //TODO(@layton): check this later when all room endpoints are made, if we can change this to documentId if needed, if not, great!
+    public ensureRoomExists(roomDocumentId: string): void {
+        if (!this._rooms.some((room) => room.documentId === roomDocumentId)) {
+            throw new DomainException(`Room does not exist`);
+        }
+    }
 
-	get documentId(): string {
-		return this._id;
-	}
+    get documentId(): string {
+        return this._documentId;
+    }
 
-	get name(): string {
-		return this._name;
-	}
+    get name(): string {
+        return this._name;
+    }
 
-	get address(): string {
-		return this._address;
-	}
+    get address(): string {
+        return this._address;
+    }
 
-	get rooms(): Room[] {
-		return this._rooms;
-	}
+    get rooms(): Room[] {
+        return this._rooms;
+    }
 }
--- src/Domain/Entities/RoomType.ts
+++ src/Domain/Entities/RoomType.ts
@@ -1,78 +1,78 @@
 import { DomainException } from "EnviroSense/Domain/mod.ts";
 
 export interface RoomTypeState {
-	documentId: string;
-	name: string;
-	icon: string;
+    documentId: string;
+    name: string;
+    icon: string;
 }
 
 export class RoomType {
-	private readonly _id: string;
-	private _name: string;
-	private _icon: string;
+    private readonly _documentId: string;
+    private _name: string;
+    private _icon: string;
 
-	private constructor(documentId: string, name: string, icon: string) {
-		this._id = documentId;
-		this._name = name;
-		this._icon = icon;
-	}
+    private constructor(documentId: string, name: string, icon: string) {
+        this._documentId = documentId;
+        this._name = name;
+        this._icon = icon;
+    }
 
-	static create(documentId: string, name: string, icon: string): RoomType {
-		const roomType = new RoomType(documentId, name, icon);
-		roomType.validateState();
+    static create(documentId: string, name: string, icon: string): RoomType {
+        const roomType = new RoomType(documentId, name, icon);
+        roomType.validateState();
 
-		return roomType;
-	}
+        return roomType;
+    }
 
-	static load(state: RoomTypeState): RoomType {
-		const roomType = new RoomType(state.documentId, state.name, state.icon);
-		roomType.validateState();
+    static load(state: RoomTypeState): RoomType {
+        const roomType = new RoomType(state.documentId, state.name, state.icon);
+        roomType.validateState();
 
-		return roomType;
-	}
+        return roomType;
+    }
 
-	public updateName(name: string): void {
-		if (!name) {
-			throw new DomainException("Name is required.");
-		}
-		this._name = name;
-	}
+    public updateName(name: string): void {
+        if (!name) {
+            throw new DomainException("Name is required.");
+        }
+        this._name = name;
+    }
 
-	public updateIcon(icon: string): void {
-		if (!icon) {
-			throw new DomainException("Icon is required.");
-		}
-		this._icon = icon;
-	}
+    public updateIcon(icon: string): void {
+        if (!icon) {
+            throw new DomainException("Icon is required.");
+        }
+        this._icon = icon;
+    }
 
-	public validateState(): void {
-		this.ensureNameIsNotEmpty();
-		this.ensureIconIsNotEmpty();
-	}
+    public validateState(): void {
+        this.ensureNameIsNotEmpty();
+        this.ensureIconIsNotEmpty();
+    }
 
-	private ensureNameIsNotEmpty(): void {
-		if (!this._name) {
-			throw new DomainException("Room type name is required.");
-		}
-	}
+    private ensureNameIsNotEmpty(): void {
+        if (!this._name) {
+            throw new DomainException("Room type name is required.");
+        }
+    }
 
-	private ensureIconIsNotEmpty(): void {
-		if (!this._icon) {
-			throw new DomainException(
-				"Room type icon is required. Enter the ID of the icon.",
-			);
-		}
-	}
+    private ensureIconIsNotEmpty(): void {
+        if (!this._icon) {
+            throw new DomainException(
+                "Room type icon is required. Enter the ID of the icon."
+            );
+        }
+    }
 
-	get documentId(): string {
-		return this._id;
-	}
+    get documentId(): string {
+        return this._documentId;
+    }
 
-	get name(): string {
-		return this._name;
-	}
+    get name(): string {
+        return this._name;
+    }
 
-	get icon(): string {
-		return this._icon;
-	}
+    get icon(): string {
+        return this._icon;
+    }
 }
