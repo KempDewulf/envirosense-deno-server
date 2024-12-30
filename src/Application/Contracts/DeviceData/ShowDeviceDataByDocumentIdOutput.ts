4d63eea80992f8819271370b083f75bda3e30731
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-12-30 19:22:33 +0100

    Remove redundant documentId property from ShowDeviceData interfaces

--- src/Application/Contracts/DeviceData/ShowDeviceDataByDocumentIdOutput.ts
+++ src/Application/Contracts/DeviceData/ShowDeviceDataByDocumentIdOutput.ts
@@ -2,7 +2,6 @@ import { AirData, Device } from "EnviroSense/Domain/mod.ts";
 
 export interface ShowDeviceDataByDocumentIdOutput {
 	documentId: string;
-	documentId: string;
 	device: Device;
 	timestamp: Date;
 	airData: AirData;
--- src/Application/Contracts/DeviceData/ShowDeviceDataOutput.ts
+++ src/Application/Contracts/DeviceData/ShowDeviceDataOutput.ts
@@ -2,7 +2,6 @@ import { AirData, Device } from "EnviroSense/Domain/mod.ts";
 
 export interface ShowDeviceDataOutput {
 	documentId: string;
-	documentId: string;
 	device: Device;
 	timestamp: Date;
 	airData: AirData;
