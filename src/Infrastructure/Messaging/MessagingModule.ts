1934997c87bdeacf0337e392c1b161a36731f0eb
Dewulf Kemp <kemp.dewulf@student.howest.be>
2025-01-01 19:53:02 +0000

    Revert "Revert "Merge branch...""
    This reverts commit a94c4ffaa3e13e29918a780256837d650b69b4d7

--- src/Infrastructure/Messaging/MessagingModule.ts
+++ src/Infrastructure/Messaging/MessagingModule.ts
@@ -2,7 +2,7 @@ import { Module } from "EnviroSense/Infrastructure/Shared/Modules/Module.ts";
 import { Messaging } from "EnviroSense/Infrastructure/Messaging/Messaging.ts";
 import { DeviceDataRepository, DeviceRepository, ProcessDeviceDataInput, UseCase } from "EnviroSense/Application/Contracts/mod.ts";
 import { ProcessDeviceData } from "EnviroSense/Application/mod.ts";
-import { DeviceDataStrapiRepository, DeviceStrapiRepository } from "EnviroSense/Infrastructure/Persistence/mod.ts";
+import { DeviceDataStrapiRepository, DeviceStrapiRepository, RoomStrapiRepository } from "EnviroSense/Infrastructure/Persistence/mod.ts";
 import { FirebaseMessaging } from "EnviroSense/Infrastructure/Messaging/FirebaseMessaging.ts";
 
 export class MessagingModule implements Module {
@@ -13,12 +13,14 @@ export class MessagingModule implements Module {
 	constructor() {
 		const deviceRepository: DeviceRepository = new DeviceStrapiRepository();
 		const deviceDataRepository: DeviceDataRepository = new DeviceDataStrapiRepository();
+		const roomRepository = new RoomStrapiRepository();
 		this.firebaseMessaging = new FirebaseMessaging();
 
 		this.processDeviceDataUseCase = new ProcessDeviceData(
 			deviceRepository,
 			deviceDataRepository,
 			this.firebaseMessaging,
+			roomRepository,
 		);
 
 		this.messaging = new Messaging(this.processDeviceDataUseCase);
