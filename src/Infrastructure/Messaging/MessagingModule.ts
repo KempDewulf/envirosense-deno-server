69bd83c64198950cac9736b850cf0f0f06e36eec
kemp.dewulf <kemp.dewulf@student.howest.be>
2025-01-02 04:23:06 +0100

    Update subscription topic in MessagingModule from "device/#" to "devices/#"

--- src/Infrastructure/Messaging/MessagingModule.ts
+++ src/Infrastructure/Messaging/MessagingModule.ts
@@ -30,6 +30,6 @@ export class MessagingModule implements Module {
 		console.log("Messaging module started");
 
 		await this.messaging.connect();
-		await this.messaging.subscribe("device/#");
+		await this.messaging.subscribe("devices/#");
 	}
 }
