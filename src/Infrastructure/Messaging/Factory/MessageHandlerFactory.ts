4d41d55ba8ecbc330d6a9198e9cc600e17d80a5c
layton <layton.berth@student.howest.be>
2025-01-04 14:03:35 +0100

    Add DeviceUiModeMessageHandler to MessageHandlerFactory for UI mode updates

--- src/Infrastructure/Messaging/Factory/MessageHandlerFactory.ts
+++ src/Infrastructure/Messaging/Factory/MessageHandlerFactory.ts
@@ -1,12 +1,14 @@
 import { DeviceDataMessageHandler, DeviceLimitMessageHandler, MessageHandler, MessagingUseCaseRegistry } from "EnviroSense/Infrastructure/Messaging/mod.ts";
+import { DeviceUiModeMessageHandler } from "EnviroSense/Infrastructure/Messaging/Handlers/DeviceUiModeMessageHandler.ts";
 
 export class MessageHandlerFactory {
     private handlers: MessageHandler[] = [];
 
     constructor(useCaseRegistry: MessagingUseCaseRegistry) {
         this.handlers = [
-            new DeviceDataMessageHandler(useCaseRegistry.processDeviceDataUseCase),
-            new DeviceLimitMessageHandler(useCaseRegistry.updateDeviceLimitUseCase)
+            new DeviceDataMessageHandler(useCaseRegistry.processDeviceDataUseCase!),
+            new DeviceLimitMessageHandler(useCaseRegistry.updateDeviceLimitUseCase!),
+            new DeviceUiModeMessageHandler(useCaseRegistry.updateDeviceUiModeUseCase!),
         ];
     }
 
