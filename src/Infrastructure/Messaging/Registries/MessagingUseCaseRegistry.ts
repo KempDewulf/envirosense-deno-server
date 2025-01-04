60317d94d7eb1ddf40af9cb55e2a3814629d4eb2
layton <layton.berth@student.howest.be>
2025-01-04 14:03:42 +0100

    Add updateDeviceUiModeUseCase to MessagingUseCaseRegistry

--- src/Infrastructure/Messaging/Registries/MessagingUseCaseRegistry.ts
+++ src/Infrastructure/Messaging/Registries/MessagingUseCaseRegistry.ts
@@ -1,6 +1,7 @@
-import { ProcessDeviceDataInput, UpdateDeviceLimitInput, UseCase } from "EnviroSense/Application/Contracts/mod.ts";
+import { ProcessDeviceDataInput, UpdateDeviceLimitInput, UpdateDeviceUiModeInput, UseCase } from "EnviroSense/Application/Contracts/mod.ts";
 
 export interface MessagingUseCaseRegistry {
 	processDeviceDataUseCase: UseCase<ProcessDeviceDataInput> | undefined;
 	updateDeviceLimitUseCase: UseCase<UpdateDeviceLimitInput> | undefined;
+	updateDeviceUiModeUseCase: UseCase<UpdateDeviceUiModeInput> | undefined;
 }
