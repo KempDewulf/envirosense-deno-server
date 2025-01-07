64c22efff3fbb822391b0d3e2b8cc295c6159b05
Layton Berth <laytonbrth@gmail.com>
2025-01-07 19:37:42 +0100

    Fix DeviceNotFoundError message and name for clarity

--- src/Infrastructure/Shared/Errors/DeviceNotFoundError.ts
+++ src/Infrastructure/Shared/Errors/DeviceNotFoundError.ts
@@ -1,6 +1,6 @@
 export class DeviceNotFoundError extends Error {
 	constructor(deviceId: string) {
-		super(`Building with ID ${deviceId} not found.`);
-		this.name = "BuildingNotFoundError";
+		super(`Device with ID ${deviceId} not found.`);
+		this.name = "DeviceNotFoundError";
 	}
 }
