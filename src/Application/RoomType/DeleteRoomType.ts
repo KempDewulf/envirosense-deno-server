0fb056574ef9ada21adbef23c03990d6bdac192c
Layton Berth <laytonbrth@gmail.com>
2025-01-07 19:42:54 +0100

    Refactor DeleteRoomType use case to throw RoomTypeNotFoundError for missing room type

--- src/Application/RoomType/DeleteRoomType.ts
+++ src/Application/RoomType/DeleteRoomType.ts
@@ -1,4 +1,5 @@
 import { DeleteRoomTypeInput, RoomTypeRepository, UseCase } from "EnviroSense/Application/Contracts/mod.ts";
+import { RoomTypeNotFoundError } from "EnviroSense/Infrastructure/Shared/mod.ts";
 
 export class DeleteRoomType implements UseCase<DeleteRoomTypeInput> {
 	private readonly _roomTypeRepository: RoomTypeRepository;
@@ -10,12 +11,9 @@ export class DeleteRoomType implements UseCase<DeleteRoomTypeInput> {
 	}
 
 	public async execute(input: DeleteRoomTypeInput): Promise<void> {
-		const roomType = (await this._roomTypeRepository.find(input.roomTypeDocumentId))
-			.orElseThrow(() =>
-				new Error(
-					`RoomType with ID ${input.roomTypeDocumentId} not found.`,
-				)
-			);
+		const roomType = (await this._roomTypeRepository.find(input.roomTypeDocumentId)).orElseThrow(() =>
+			new RoomTypeNotFoundError(input.roomTypeDocumentId)
+		);
 
 		await this._roomTypeRepository.deleteEntity(roomType);
 	}
