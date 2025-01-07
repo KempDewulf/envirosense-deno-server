e4ab59781484e07e24e79106d8aee2b7ce4febe4
Layton Berth <laytonbrth@gmail.com>
2025-01-07 18:42:56 +0100

    Refactor ShowDeviceByDocumentId use case to throw DeviceNotFoundError for missing devices

--- src/Application/Device/ShowDeviceByDocumentId.ts
+++ src/Application/Device/ShowDeviceByDocumentId.ts
@@ -6,6 +6,7 @@ import {
 	ShowDeviceByDocumentIdOutput,
 	UseCase,
 } from "EnviroSense/Application/Contracts/mod.ts";
+import { DeviceNotFoundError } from "EnviroSense/Infrastructure/Shared/Errors/DeviceNotFoundError.ts";
 
 export class ShowDeviceByDocumentId implements UseCase<ShowDeviceByDocumentIdInput> {
 	private readonly _outputPort: OutputPort<ShowDeviceByDocumentIdOutput>;
@@ -20,15 +21,11 @@ export class ShowDeviceByDocumentId implements UseCase<ShowDeviceByDocumentIdInp
 	}
 
 	public async execute(input: ShowDeviceByDocumentIdInput): Promise<void> {
-		const deviceOptional = await this._deviceRepository.find(
-			input.deviceDocumentId,
+		const deviceQueryDto = (await this._deviceRepository.find(input.deviceDocumentId)).orElseThrow(() =>
+			new DeviceNotFoundError(input.deviceDocumentId)
 		);
 
-		const deviceDto = deviceOptional.orElseThrow(
-			() => new Error(`Device with ID ${input.deviceDocumentId} not found.`),
-		);
-
-		const device = this.mapDtoToOutput(deviceDto);
+		const device = this.mapDtoToOutput(deviceQueryDto);
 		this._outputPort.present(device);
 	}
 
